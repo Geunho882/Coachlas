00ae694439d45412433a8aff623cbfe311ac31a7
Geunho <erich54656@gmail.com>
2025-06-17 03:39:21 +1200

    Home and Learn page commit

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
+let opensquats = document.querySelector(".exercise-box");
+let openedsquat = document.querySelector("#squatexercise");
+let popupOverlay = document.querySelector(".popup-overlay");
+let closesquat = document.querySelector(".closepopup");
 
+opensquats.addEventListener('click', () => {
+  popupOverlay.style.display = "block";
+  openedsquat.classList.add('open');
+});
+
+closesquat.addEventListener('click',()=>{
+    openedsquat.classList.remove('open');
+    popupOverlay.style.display = "none";
+});
